7f30ce5ca199b7d85737330d7f689ac838e69b07
Vaibhav Agarwal <vjindal742@gmail.com>
2025-04-10 10:49:31 +0530

    image change

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -5,9 +5,9 @@ const Page = () => {
   return (
     <div className="w-full min-h-screen bg-black flex flex-col items-center gap-y-10">
       <Image className={"w-[50vw]"} src={"1.jpg"} />
-      <Image className={"w-[50vw]"} src={"1.jpg"} />
-      <Image className={"w-[50vw]"} src={"1.jpg"} />
-      <Image className={"w-[50vw]"} src={"1.jpg"} />
+      <Image className={"w-[50vw]"} src={"2.jpg"} />
+      <Image className={"w-[50vw]"} src={"3.jpg"} />
+      <Image className={"w-[50vw]"} src={"4.jpg"} />
     </div>
   );
 };
